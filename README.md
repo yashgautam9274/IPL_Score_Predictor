846f8eb26a3fca991f3b4235f0886e5fb6003148
Yash <yash9274gautam@gmail.com>
2022-05-03 03:14:05 +0530

    done

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# IPL_Score_Predictor
+# IPL Score Predictor :india:
+![GIF](readme_resources/ipl_teams.jpg)
 
-Web app link: https://tata-ipl-score-predictor.herokuapp.com/
+#### The IPL Score Predictor project is about predicting score of batting team.
+#### This web application have IPL 2022 teams where you can predict score of batting team.
+
+## Overview
+- I have IPL matches records from year 2008 to 2020.
+- I have trained machine learning model for 8 teams which were playing in year 2021.
+- Here i have performed hyperparameter tunning on various model and i got **Random Forest** as best model for this problem statement.
+- Gradient Booost algorithm gives **0.850539 r2_score** & **1.632532 MAE** on train dataset and **0.871335 r2_score** & **6.438084 MAE** on test dataset.
+- Random Forest algorithm gives **0.672894 r2_score** & **11.296707 MAE** on train dataset and **0.681484 r2_score** & **12.173585 MAE** on test dataset.
+- Difference of train dataset MAE and test dataset MAE is **4.805552** for Gradient Boost.
+- Difference of train dataset MAE and test dataset MAE is **0.876878** for Random Forest.
+- Here accuracy or **r2_score** of Gradient Boost algorithm is higher than Random Forest but Random Forest's **MAE** difference between train and test dataset is lower than Gradient Boost.
+- If i select Gradient Boost algorithm which has high r2_score then our model will **not perform stable** as it performed on train dataset because **MAE** difference between train and test dataset is **high** that's why it might generate **overfitting** issue.
+- If i select Radom Forest algorithm which does not have high r2_score still it will perfrom **stable** as it performed on train dataset because MAE difference between train and test dataset is **low**.
+- Here i have selected **Random Forest** algorithm with it's hyperparamter which best fit for this problem statement with around **68%** of accuracy.
+- Application developed using Flask, Javascript, Bootstrap, CSS and HTML
+
+## Data Source
+- In this project i have used IPL dataset from kaggle, you can get it from [here](https://www.kaggle.com/patrickb1912/ipl-complete-dataset-20082020)
+
+
+## How to use
+- Select venue, batting team, bowling tean and fill the inputs with proper information then click on predict button you will get predicted score of batting team.
+
+## Technologies Used
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"></code>
+<code><img height="30" src="https://github.com/tomchen/stack-icons/raw/master/logos/bootstrap.svg"></code>
+<code><img height="30" src="https://miro.medium.com/max/876/1*0G5zu7CnXdMT9pGbYUTQLQ.png"></code>
+<code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/e/ec/Heroku_logo.svg"></code>
+
+<code><img height="30" src="https://raw.githubusercontent.com/numpy/numpy/7e7f4adab814b223f7f917369a72757cd28b10cb/branding/icons/numpylogo.svg"></code>
+<code><img height="30" src="https://matplotlib.org/_static/logo2.svg"></code>
+<code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/330px-Scikit_learn_logo_small.svg.png"></code>
+
+
+## 
+- If you like my work and it helped you in anyway then please do ⭐ the repository it will motivate me to make more amazing projects
--- app.py
+++ app.py
@@ -1,73 +1,35 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
+from flask import Flask, request, render_template
+import util
 
-app=Flask(__name__,template_folder='template')
+app = Flask(__name__)
 
-filename = 'first-innings-score-lr-model.pkl'
-regressor = pickle.load(open(filename, 'rb'))
+teams, venues = util.load_artifacts()
+teams.sort()
+venues.sort()
 
-@app.route('/')
+@app.route("/")
 def home():
-   return render_template('index.html')
+    return render_template("home.html", team = teams, venue = venues)
 
+@app.route("/predictedScore", methods = ["POST"])
+def predicted_score():
+    try:
+        venue = request.form["venue"]
+        bat_team = request.form["bat_team"]
+        bowl_team = request.form["bowl_team"]
+        overs = float(request.form["overs"])
+        runs = int(request.form["runs"])
+        wickets = int(request.form["wickets"])
+        runs_last_5 = int(request.form["runs_last_5"])
+        wickets_last_5 = int(request.form["wickets_last_5"])
 
-@app.route('/result', methods=['POST'])
-def result():
-   temp_array = list()
-    
-   if request.method == 'POST':
-        
-        batting_team = request.form['batting-team']
-        if batting_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
-        elif batting_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
-        elif batting_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
-        elif batting_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
-        elif batting_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif batting_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
-        elif batting_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
-        elif batting_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
-            
-            
-        bowling_team = request.form['bowling-team']
-        if bowling_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
-        elif bowling_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
-        elif bowling_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
-        elif bowling_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
-        elif bowling_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif bowling_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
-        elif bowling_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
-        elif bowling_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
-            
-            
-        overs = float(request.form['overs'])
-        runs = int(request.form['runs'])
-        wickets = int(request.form['wickets'])
-        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
-        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
-        
-        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
-        
-        data = np.array([temp_array])
-        my_prediction = int(regressor.predict(data)[0])
-   return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
-   
+        result = int(util.predict_score(overs, wickets, runs, wickets_last_5, runs_last_5, bat_team, bowl_team, venue))
+        if result == 1:
+            return "Something is wrong please fill proper input!!"
+        else:
+            return "Predicted score range of " + bat_team + " is between " + str(result - 5) + " to " + str(result + 5)
+    except :
+        return "Something is wrong please fill proper input!!"
 
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    app.run()
